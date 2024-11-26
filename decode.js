bceb58168899af86fdf5bd2cb5456d3e453a9983
Starbuck7410 <starbuck7410@gmail.com>
2024-11-26 12:48:32 +0200

    added simple file attachment handling

--- decode.js
+++ decode.js
@@ -7,8 +7,23 @@ function decodeChatMessage(message, users) {
 
 
         // Extracting fields
+        // [{"_id":"01J2EXQ8QW2761NS61DK9ZEM0D","tag":"attachments","filename":"welcome_to_the_ai_automation_agency_campus.mp3","metadata":{"type":"Audio","duration":1563},"content_type":"audio/mpeg","size":26569686}]
+        let attachments = message.attachments || [];
+        let attachmentString = '';
+        for (let i = 0; i < attachments.length; i++) {
+            let formattedSize = (attachments[i].size / 1024 / 1024).toFixed(2) + "MB";
+            let metadata = "";
+            for (key in attachments[i].metadata) {
+                if (key == 'placeholder') {
+                    continue;
+                }
+                metadata += `${key} - ${attachments[i].metadata[key]}, `;
+            }
+            attachmentString += `::${attachments[i].filename} - ${formattedSize} (${metadata})\n`;
+        }
+        console.log(attachmentString);
         let author = message.author;
-        let content = message.content;
+        let content = message.content || "";
         let timestamp = new Date(message.timestamp).toLocaleString(); // Format timestamp
         let reactionCounts = '';
         if (message.reaction_counts) {
@@ -18,12 +33,12 @@ function decodeChatMessage(message, users) {
         }
         let k = 0;
 
-        let mentions = content.match(/<@.*?>/g);
+        let mentions = content.match(/<@.{26}?>/g);
         if (!mentions) {
             mentions = [];
         }
         for (let k = 0; k < mentions.length; k++) {
-            console.log(mentions[k]);
+            // console.log(mentions[k]);
             let username = findUsername(mentions[k].slice(2, -1), users);
             content = content.replace(mentions[k], `@${username}`);
         }
@@ -32,6 +47,8 @@ function decodeChatMessage(message, users) {
         return `
 --- ${findUsername(author, users)}, $ ${timestamp} ---
 ${content}
+
+${attachmentString}
 
 /${reactionCounts || ""}/
 `;
@@ -43,9 +60,10 @@ ${content}
 
 function findUsername(id, users) {
     for (let i = 0; i < users.length; i++) {
-        if (users[i].user._id == id) {
+        if (users[i].user._id == id || users[i].member._id.user == id) {
             return users[i].user.username;
         }
     }
-    return 'Unknown';
+    // console.log("Unknown user: " + id);
+    return 'Unknown'; // TODO handle unknown users better
 }
